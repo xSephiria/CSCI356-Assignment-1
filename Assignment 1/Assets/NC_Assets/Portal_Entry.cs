5195679789f775fec927573d1eb98d925c33b93c
HighwindEasy <Kiraarmy15@gmail.com>
2021-08-23 00:10:58 +0800

    Last changes made

--- Assignment 1/Assets/NC_Assets/Portal_Entry.cs
+++ Assignment 1/Assets/NC_Assets/Portal_Entry.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class Portal_Entry : MonoBehaviour
 {
-
+    public GameObject Player;
     public int P_ID;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
@@ -18,22 +18,20 @@ public class Portal_Entry : MonoBehaviour
         
     }
 
-    private void OnCollisionEnter(Collision collision)
+    void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag == "Player")
         {
-            GameObject[] target_to_go = GameObject.FindGameObjectsWithTag("Portals_Exit");
-            Debug.Log(target_to_go.Length);
-            for (int i = 0; i < target_to_go.Length; i++)
+            //Player.gameObject.transform.position = Target.gameObject.transform.position + new Vector3(0, 3, 0);
+            GameObject[] targets = GameObject.FindGameObjectsWithTag("Portals_Exit");
+            for(int i = 0; i < targets.Length; i++)
             {
-                if (target_to_go[i].GetComponent<Portal_Exit>().P_EID == P_ID)
+                if(targets[i].GetComponent<Portal_Exit>().P_EID == P_ID)
                 {
-                    Transform PlaterTrf;
-                    PlaterTrf = GameObject.FindGameObjectWithTag("Player").transform;
-                    PlaterTrf = target_to_go[i].gameObject.transform;
+                    Player.gameObject.transform.position = targets[i].gameObject.transform.position + new Vector3(0, 3, 0);
                 }
-
             }
+            
         }
     }
 }
